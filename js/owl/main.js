835d9079799f61fb8730df010016098d8c2e7dc2
Maria Eduarda <mariaed.cassiano@gmail.com>
2021-02-27 21:24:42 -0300

    projeto finalizado

---
+++ js/owl/main.js
@@ -0,0 +1,33 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
